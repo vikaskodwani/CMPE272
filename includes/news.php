653a722618eccd83706f2b180e4fb16564e7a573
Vik <vkodwani@salesforce.com>
2018-03-14 11:53:03 -0700

    update

--- includes/news.php
+++ includes/news.php
@@ -2,10 +2,10 @@
 	<div class="container fullsize">
 	    <h1 class="animated" align="center">News</h1>
 		<div class="flex-container-news">
-			  <div><a  title="Progress with us produly support Tech Community" href="#"><img src="../images/new1.jpg"/>Progress with us produly support Tech Community</a></div>  
-			  <div><a  title="Introducing Progress App" href="#"><img src="../images/new2.png"/>Introducing Progress App</a></div>
-			   <div><a  title="Introducing Progress App" href="#"><img src="../images/new2.png"/>Introducing Progress App</a></div>
-				<div><a  title="Introducing Progress App" href="#"><img src="../images/new2.png"/>Introducing Progress App</a></div>
+			  <div><a  title="Introducing Progresswithus App" href="#"><img src="../images/news2.png"/>Introducing Progress App</a></div>
+			   <div><a  title="Progresswithus Launches AI-Driven Chatbot" href="#"><img src="../images/news3.jpg"/>Progresswithus Launches AI-Driven Chatbot</a></div>
+				<div><a  title="Progresswithus partners with AWS" href="#"><img src="../images/news4.jpg"/>Progresswithus partners with AWS</a></div>
+<div><a  title="Progress with us produly support Tech Community" href="#"><img src="../images/news1.jpg"/>Progress with us produly support Tech Community</a></div>
             </div>
 	</div>
 </section>
