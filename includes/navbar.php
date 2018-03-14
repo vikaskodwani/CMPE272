8e0ccae4c1b77fc797458a385ec93df9e02f7a68
Vik <vkodwani@salesforce.com>
2018-03-13 19:39:57 -0700

    updates img to images

--- includes/navbar.php
+++ includes/navbar.php
@@ -3,7 +3,7 @@
         <ul class="main-nav">
         	<table>
         	 <tr>
-        	    <td><li><a href="#"><p align="left"><img border="0" src="../img/logo.png"></a></li></td>
+        	    <td><li><a href="#"><p align="left"><img border="0" src="../images/logo.png"></a></li></td>
         	    <td> <li><a href="#about">About</a></li></td>
         		<td><li><a href="#services">Services</a></li></td>
     			<td><li><a href="#news">News</a></li></td>
--- includes/news.php
+++ includes/news.php
@@ -2,8 +2,8 @@
 	<div class="container fullsize">
 	    <h1 class="animated" align="center">News</h1>
 		<div class="flex-container" >
-			  <div><a  title="International Expansion Advice"><img src="../img/new1.jpg"/>Progress with us produly support Tech Community</a></div>  
-			  <div><a  title="Competition Analysis"><img src="../img/new2.png"/>Introducing Progress App</a></div>
+			  <div><a  title="International Expansion Advice"><img src="../images/new1.jpg"/>Progress with us produly support Tech Community</a></div>  
+			  <div><a  title="Competition Analysis"><img src="../images/new2.png"/>Introducing Progress App</a></div>
 
             </div>
 	</div>
