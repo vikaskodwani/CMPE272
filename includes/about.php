d05113d9a4b169fa5476c987405f0a70860484ae
Vik <vkodwani@salesforce.com>
2018-03-14 12:19:36 -0700

    update

--- includes/about.php
+++ includes/about.php
@@ -1,8 +1,8 @@
     <section class="header" id="about">
          <div class="container">
              <br>
-                <h1 class="animated fadeInDown delay-07s">Progress With Us !! </h1>
-                <ul class="we-create animated fadeInUp delay-07s">
+                <h1 class="animated fadeInDown">Progress With Us !! </h1>
+                <ul class="we-create animated fadeInUp">
                  <h3>We provide clients with exclusive and tailored recommendations that leads to increased profits and  growth.</h3>
                 </ul>
             </div>
