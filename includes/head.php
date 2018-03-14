1c5fc532cf105489943697689e90e3dba8fea8e7
Vik <vkodwani@salesforce.com>
2018-03-13 21:06:37 -0700

    update

--- includes/head.php
+++ includes/head.php
@@ -5,17 +5,12 @@
 <link rel="icon" href="favicon.png" type="image/png">
 <link rel="shortcut icon" href="favicon.ico" type="img/x-icon">
 
-<link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
-<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,800italic,700italic,600italic,400italic,300italic,800,700,600' rel='stylesheet' type='text/css'>
-
 <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
-<link href="css/loginstyle.css" rel="stylesheet" type="text/css">
 <link href="css/style.css" rel="stylesheet" type="text/css">
 <link href="css/font-awesome.css" rel="stylesheet" type="text/css">
 <link href="css/responsive.css" rel="stylesheet" type="text/css">
 <link href="css/animate.css" rel="stylesheet" type="text/css">
 <link href="css/flex.css" rel="stylesheet" type="text/css">
-
 
 <script type="text/javascript" src="js/jquery.1.8.3.min.js"></script>
 <script type="text/javascript" src="js/bootstrap.js"></script>
--- includes/navbar.php
+++ includes/navbar.php
@@ -1,4 +1,4 @@
-<header>
+<!-- <header>
 	<div class="container">
         <ul class="main-nav">
         	<table>
@@ -13,3 +13,18 @@
         </ul>
     </div>
 </header>
+ -->
+
+
+<nav class="main-nav-outer" id="test">
+    <div class="container">
+        <ul class="main-nav">
+            <li><a><img border="0" src="../images/logo.png"></a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#services">Services</a></li>
+            <li><a href="#news">News</a></li>
+            <li><a href="#contacts">Contacts</a></li>
+        </ul>
+        <a class="res-nav_click" href="#"><i class="fa-bars"></i></a>
+    </div>
+</nav>
