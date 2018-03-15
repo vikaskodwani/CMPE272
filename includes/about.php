f6f1f563104fe15c22f3431c455dd726ee393f1a
Vik <vkodwani@salesforce.com>
2018-03-14 19:21:34 -0700

    update

--- includes/about.php
+++ includes/about.php
@@ -1,8 +1,8 @@
 <section class="header" id="about">
      <div class="container">
          <br>
-            <h1 class="animated">Progress With Us !! </h1>
-            <ul class="we-create animated">
+            <h1>Progress With Us !! </h1>
+            <ul>
              <h3>We provide clients with exclusive and tailored recommendations that leads to increased profits and  growth.</h3>
             </ul>
      </div>
--- includes/home.php
+++
@@ -1,4 +0,0 @@
-<section class="header" id="header">
-               Progerss With Us !! 
-    	       We provide clients with exclusive and tailored financial recommendations that leads to increased profits and  growth.
-</section>
