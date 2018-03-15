ffdb712d744d58b2226f52909205ab624941232f
Vik <vkodwani@salesforce.com>
2018-03-14 19:00:43 -0700

    update

--- includes/about.php
+++ includes/about.php
@@ -1,9 +1,9 @@
-    <section class="header" id="about">
-         <div class="container">
-             <br>
-                <h1 class="animated">Progress With Us !! </h1>
-                <ul class="we-create animated">
-                 <h3>We provide clients with exclusive and tailored recommendations that leads to increased profits and  growth.</h3>
-                </ul>
-            </div>
-        </section>
+<section class="header" id="about">
+     <div class="container">
+         <br>
+            <h1 class="animated">Progress With Us !! </h1>
+            <ul class="we-create animated">
+             <h3>We provide clients with exclusive and tailored recommendations that leads to increased profits and  growth.</h3>
+            </ul>
+     </div>
+</section>
