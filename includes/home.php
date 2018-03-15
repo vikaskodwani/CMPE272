4e0a6df24d39f300fdb5afbae078269b5489a67d
Vik <vkodwani@salesforce.com>
2018-03-14 19:09:54 -0700

    update

--- includes/home.php
+++ includes/home.php
@@ -1,5 +1,5 @@
 <section class="header" id="header">
-    <div class="container">
+    <div>
             <h1>Progerss With Us !! </h1>
             <ul class="we-create animated">
     	       We provide clients with exclusive and tailored financial recommendations that leads to increased profits and  growth.
