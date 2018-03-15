6038842cf9976e5ce43fe387ad261087503091e5
Vik <vkodwani@salesforce.com>
2018-03-14 19:51:21 -0700

    update

--- includes/about.php
+++ includes/about.php
@@ -1,5 +1,5 @@
 <section class="header" id="about">
-
+	 <br>
              <h1>We provide clients with exclusive and tailored recommendations that leads to increased profits and  growth.</h1>
              <br>
 
