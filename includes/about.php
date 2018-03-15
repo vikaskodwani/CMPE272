5f8384998c9b3670d7f9c9ebd4a2f0406f11ab43
Vik <vkodwani@salesforce.com>
2018-03-14 18:46:48 -0700

    update

--- includes/about.php
+++ includes/about.php
@@ -1,8 +1,8 @@
     <section class="header" id="about">
          <div class="container">
              <br>
-                <h1 class="animated fadeInDown">Progress With Us !! </h1>
-                <ul class="we-create animated fadeInUp">
+                <h1 class="animated">Progress With Us !! </h1>
+                <ul class="we-create animated">
                  <h3>We provide clients with exclusive and tailored recommendations that leads to increased profits and  growth.</h3>
                 </ul>
             </div>
--- includes/navbar.php
+++ includes/navbar.php
@@ -1,21 +1,3 @@
-<!-- <header>
-	<div class="container">
-        <ul class="main-nav">
-        	<table>
-        	 <tr>
-        	    <td><li><a href="#"><p align="left"><img border="0" src="../images/logo.png"></a></li></td>
-        	    <td> <li><a href="#about">About</a></li></td>
-        		<td><li><a href="#services">Services</a></li></td>
-    			<td><li><a href="#news">News</a></li></td>
-				<td><li><a href="#contacts">Contacts</a></li></td>
-			</tr>
-		  </table>
-        </ul>
-    </div>
-</header>
- -->
-
-
 <nav class="main-nav-outer" id="test">
     <div class="container">
         <ul class="main-nav">
