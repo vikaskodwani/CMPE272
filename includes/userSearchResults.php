74fbcd083d0246f3f27e78f86562726b3cd956c4
Vik Kodwani <34690118+vkodwani1@users.noreply.github.com>
2018-05-15 20:44:42 -0700

    Update userSearchResults.php

--- includes/userSearchResults.php
+++ includes/userSearchResults.php
@@ -32,10 +32,11 @@ $searchtext = $_POST['searchtext'];
 // $myPassword = "admin";   // use your real login password
 // $myDataBaseName = "labProject"; // use your real database name
 
-$myHost = "us-cdbr-iron-east-05.cleardb.net"; 
-$myUserName = "bde8984a4db9c4";  
-$myPassword = "d7290143";   
-$myDataBaseName = "heroku_03a0031248e6bd9"; 
+$myHost = "XXXX"; 
+$myUserName = "XXX";  
+$myPassword = "XX";   
+$myDataBaseName = "XXXX"; 
+
 $con = mysqli_connect( "$myHost", "$myUserName", "$myPassword", "$myDataBaseName" );
 
 switch ($radioval) {
